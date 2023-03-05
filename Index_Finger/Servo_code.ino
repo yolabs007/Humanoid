f25acc10c6004d1a2d7609fc6cd6fe6d077b46f8
YoLabs <59382666+yolabs007@users.noreply.github.com>
2023-03-05 12:16:40 +0530

    Create Servo_code.ino

---
+++ Index_Finger/Servo_code.ino
@@ -0,0 +1,46 @@
+/* Move the INDEX finger 
+  This code is written to check if the index finger assembly is working to ints perfection. 
+  
+  Connections 
+  Servo  -            ESP32 
+  Brown -             Ground
+  Red   -             3v3
+  Orange/Yellow  -    Digital pin - D4 in this code 
+  
+  NOTE : If your servo need to take a good load or if you are connecting multiple servo,  
+  you will  need external power supply with/without servo controller board               
+  
+  by Rahul Sharma for Yolabs 
+  This example code is in the public domain.
+  https://www.yolabs.in
+*/
+
+#include <Servo.h>
+
+Servo servo_2;  //create a servo object
+
+int angle = 0;   // servo position in degrees 
+
+int atrest_angle = 0;
+int atmid_angle = 90;
+int atclose_angle = 180;
+
+ 
+
+void setup()
+{
+  servo_2.attach(4);   //attach a pin to servo 
+
+}
+
+
+void loop() 
+{ 
+                            
+    servo_2.write(atrest_angle);               
+    delay(3000);
+    servo_2.write(atmid_angle);               
+    delay(3000);
+    servo_2.write(atclose_angle);               
+    delay(3000);
+} 
